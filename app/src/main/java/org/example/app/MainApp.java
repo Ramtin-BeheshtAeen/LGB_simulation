4877a3b2d829a07d3659d309081614dbcab0e5f7
ramtin <ramtinba145822@gmail.com>
2026-03-30 15:30:43 +0200

    working on the physics enginee

--- app/src/main/java/org/example/app/MainApp.java
+++ app/src/main/java/org/example/app/MainApp.java
@@ -31,8 +31,6 @@ import java.awt.Dimension;
 public class MainApp { 
 
     public static void main(String[] args) {
-        //**!** */ 1. Start JavaFX toolkit first
-       // Platform.startup(() -> {});
         LaserGuidance3D app = new LaserGuidance3D(); // Start THIS class
         
         //javaFX Panel Setting
@@ -40,29 +38,7 @@ public class MainApp {
         settings.setFrameRate(60);
         
         app.setSettings(settings);
-        // Use Canvas mode to allow overlaying
-        //createCanvas() + startCanvas() won't work with jme3-lwjgl3
-        //app.createCanvas(); 
-	//
-        //app.startCanvas();
 	
-	//-- Right Column: control Panel --------------------------------------
-//	SimPanel panel = new SimPanel();
- //	app.setPanel(panel);
- //	panel.setBorder(new MatteBorder(0, 1, 0, 0, new color(45, 45, 58)));
-
-
-        //app.start();//start after setting up
-    
-
-    // 3. Open JavaFX window separately
- //     Platform.runLater(() -> {
- //             Stage stage = new Stage();
- //             Button btn = new Button("Launch");
- //             stage.setScene(new Scene(new VBox(btn), 300, 600));
- //             stage.show();
- //         });
- //     }
 	SwingUtilities.invokeLater( () -> {
 	    try {UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName()); }
 	    catch (Exception ignored) {}
---
+++ app/src/main/java/org/example/app/Physics.java
@@ -0,0 +1,12 @@
+public class Physics{
+
+    public double mass        = 250.0;  // kg
+    public double dragCoeff   = 0.42;   // Cd
+    public double area        = 0.25;   // reference area m^2
+    public double launchVel   = 210.0;  // m/s
+    public double launchAngel = -10.0; //degrees (negative means diving)
+    public double windx       = 0.0;  // m/s east
+
+
+
+}
